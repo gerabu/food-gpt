ddea3388643f87110d12a3a9fbc0c94a52053898
“gerabu” <aburtogerald@gmail.com>
2025-03-17 18:44:36 -0600

    :fire: Remove posts related stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,16 @@
-import { LatestPost } from "@/app/_components/post";
 import { auth } from "@/server/auth";
-import { api, HydrateClient } from "@/trpc/server";
+import { HydrateClient } from "@/trpc/server";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
   const session = await auth();
 
   if (session?.user) {
-    void api.post.getLatest.prefetch();
   }
 
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center">
-        <h1>{hello.greeting}</h1>
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          {session?.user && <LatestPost />}
-        </div>
+        <h1>Hello there 👋</h1>
       </main>
     </HydrateClient>
   );
